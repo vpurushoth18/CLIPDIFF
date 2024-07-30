e7798236677ae7f3ca34dba091956a9b4649ddd7
vpurushoth18 <vpurushoth97@gmail.com>
2024-07-30 15:05:38 +0530

    edited argument parsing

--- main_task_caption.py
+++ main_task_caption.py
@@ -84,42 +84,38 @@ class EvalCap(COCOEvalCap):
 
 def get_args(description='Teabased on Captioning Task'):
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("--do_pretrain", action='store_true', help="Whether to run training.")
-    parser.add_argument("--do_train", action='store_true', help="Whether to run training.")
-    parser.add_argument("--do_eval", action='store_true', help="Whether to run eval on the dev set.")
+    parser.add_argument("--do_pretrain", action='store_true')
+    parser.add_argument("--do_train", action='store_true')
+    parser.add_argument("--do_eval", action='store_true')
 
-    parser.add_argument('--data_path', type=str, default='data/datatype', help='data file path')
-    parser.add_argument('--features_path', type=str, default='data/datatype/images', help='feature path')
+    parser.add_argument('--data_path', type=str, default='data/datatype')
+    parser.add_argument('--features_path', type=str, default='data/datatype/images')
 
-    parser.add_argument('--num_thread_reader', type=int, default=1, help='')
-    parser.add_argument('--lr', type=float, default=0.0001, help='initial learning rate')
-    parser.add_argument('--epochs', type=int, default=20, help='upper epoch limit')
-    parser.add_argument('--batch_size', type=int, default=256, help='batch size')
-    parser.add_argument('--batch_size_val', type=int, default=3500, help='batch size eval')
-    parser.add_argument('--lr_decay', type=float, default=0.9, help='Learning rate exp epoch decay')
-    parser.add_argument('--n_display', type=int, default=100, help='Information display frequence')
-    parser.add_argument('--seed', type=int, default=42, help='random seed')
-    parser.add_argument('--max_words', type=int, default=20, help='')
-    parser.add_argument('--margin', type=float, default=0.1, help='margin for loss')
-    parser.add_argument('--hard_negative_rate', type=float, default=0.5, help='rate of intra negative sample')
-    parser.add_argument('--negative_weighting', type=int, default=1, help='Weight the loss for intra negative')
-    parser.add_argument('--n_pair', type=int, default=1, help='Num of pair to output from data loader')
+    parser.add_argument('--num_thread_reader', type=int, default=1)
+    parser.add_argument('--lr', type=float, default=0.0001)
+    parser.add_argument('--epochs', type=int, default=20)
+    parser.add_argument('--batch_size', type=int, default=256)
+    parser.add_argument('--batch_size_val', type=int, default=3500)
+    parser.add_argument('--lr_decay', type=float, default=0.9)
+    parser.add_argument('--n_display', type=int, default=100)
+    parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--max_words', type=int, default=20)
+    parser.add_argument('--margin', type=float, default=0.1)
+    parser.add_argument('--hard_negative_rate', type=float, default=0.5)
+    parser.add_argument('--negative_weighting', type=int, default=1)
+    parser.add_argument('--n_pair', type=int, default=1)
 
-    parser.add_argument("--output_dir", default=None, type=str, required=True,
-                        help="The output directory where the model predictions and checkpoints will be written.")
-    parser.add_argument("--cross_model", default="cross-base", type=str, required=False, help="Cross module")
-    parser.add_argument("--decoder_model", default="decoder-base", type=str, required=False, help="Decoder module")
-    parser.add_argument("--init_model", default=None, type=str, required=False, help="Initial model.")
-    parser.add_argument("--resume_model", default=None, type=str, required=False, help="Resume train model.")
-    parser.add_argument("--do_lower_case", action='store_true', help="Set this flag if you are using an uncased model.")
-    parser.add_argument("--warmup_proportion", default=0.1, type=float,
-                        help="Proportion of training to perform linear learning rate warmup for. E.g., 0.1 = 10%% of training.")
-    parser.add_argument('--gradient_accumulation_steps', type=int, default=1,
-                        help="Number of updates steps to accumulate before performing a backward/update pass.")
-    parser.add_argument('--n_gpu', type=int, default=1, help="Changed in the execute process.")
+    parser.add_argument("--output_dir", default=None, type=str, required=True)
+    parser.add_argument("--cross_model", default="cross-base", type=str, required=False)
+    parser.add_argument("--decoder_model", default="decoder-base", type=str, required=False)
+    parser.add_argument("--init_model", default=None, type=str, required=False)
+    parser.add_argument("--resume_model", default=None, type=str, required=False)
+    parser.add_argument("--do_lower_case", action='store_true')
+    parser.add_argument("--warmup_proportion", default=0.1, type=float)
+    parser.add_argument('--gradient_accumulation_steps', type=int, default=1)
+    parser.add_argument('--n_gpu', type=int, default=1)
 
-    parser.add_argument("--cache_dir", default="", type=str,
-                        help="Where do you want to store the pre-trained models downloaded from s3")
+    parser.add_argument("--cache_dir", default="", type=str)
 
     # parser.add_argument('--fp16', action='store_true',
                         # help="Whether to use 16-bit (mixed) precision (through NVIDIA apex) instead of 32-bit")
@@ -127,28 +123,26 @@ def get_args(description='Teabased on Captioning Task'):
     #                     # help="For fp16: Apex AMP optimization level selected in ['O0', 'O1', 'O2', and 'O3']."
                             #  "See details at https://nvidia.github.io/apex/amp.html")
 
-    parser.add_argument("--task_type", default="caption", type=str, help="Point the task `retrieval` or `caption` to finetune.")
-    parser.add_argument("--datatype", default="msrvtt", type=str, help="Point the dataset to finetune.")
+    parser.add_argument("--task_type", default="caption", type=str)
+    parser.add_argument("--datatype", default="tea", type=str)
 
-    parser.add_argument("--world_size", default=0, type=int, help="distribted training")
-    parser.add_argument("--local_rank", default=0, type=int, help="distribted training")
-    parser.add_argument("--rank", default=0, type=int, help="distribted training")
-    parser.add_argument('--coef_lr', type=float, default=1., help='coefficient for bert branch.')
-    parser.add_argument('--use_mil', action='store_true', help="Whether use MIL as Miech et. al. (2020).")
-    parser.add_argument('--sampled_use_mil', action='store_true', help="Whether MIL, has a high priority than use_mil.")
+    parser.add_argument("--world_size", default=0, type=int)
+    parser.add_argument("--local_rank", default=0, type=int)
+    parser.add_argument("--rank", default=0, type=int)
+    parser.add_argument('--coef_lr', type=float, default=1)
+    parser.add_argument('--use_mil', action='store_true')
+    parser.add_argument('--sampled_use_mil', action='store_true')
 
-    parser.add_argument('--text_num_hidden_layers', type=int, default=12, help="Layer NO. of text.")
-    parser.add_argument('--visual_num_hidden_layers', type=int, default=12, help="Layer NO. of visual.")
-    parser.add_argument('--intra_num_hidden_layers', type=int, default=9, help="Layer NO. of intra module")
-    parser.add_argument('--cross_num_hidden_layers', type=int, default=2, help="Layer NO. of cross.")
+    parser.add_argument('--text_num_hidden_layers', type=int, default=12)
+    parser.add_argument('--visual_num_hidden_layers', type=int, default=12)
+    parser.add_argument('--intra_num_hidden_layers', type=int, default=9)
+    parser.add_argument('--cross_num_hidden_layers', type=int, default=2)
 
-    parser.add_argument('--freeze_layer_num', type=int, default=0, help="Layer NO. of CLIP need to freeze.")
-    parser.add_argument('--linear_patch', type=str, default="2d", choices=["2d", "3d"],
-                        help="linear projection of flattened patches.")
+    parser.add_argument('--freeze_layer_num', type=int, default=0)
+    parser.add_argument('--linear_patch', type=str, default="2d", choices=["2d", "3d"])
 
-    parser.add_argument("--pretrained_clip_name", default="ViT-B/32", type=str, help="Choose a CLIP version")
-    parser.add_argument("--gt_dir", default="gt", type=str,
-                        help="The output directory where the model predictions and checkpoints will be written.")
+    parser.add_argument("--pretrained_clip_name", default="ViT-B/32", type=str)
+    parser.add_argument("--gt_dir", default="gt", type=str)
 
     args = parser.parse_args()
 
@@ -315,7 +309,7 @@ def train_epoch(epoch, args, model, train_dataloader, device, n_gpu, optimizer, 
         image_name = batch[-1]
         batch = tuple(t.to(device=device, non_blocking=True) for t in batch[:-1])
 
-        if args.datatype == "clevr":
+        if args.datatype == "tea":
             input_ids, input_mask, segment_ids, bef_image, aft_image, no_image, image_mask, \
             pairs_input_caption_ids, pairs_decoder_mask, pairs_output_caption_ids, \
             no_pairs_input_caption_ids, no_pairs_decoder_mask, no_pairs_output_caption_ids = batch
