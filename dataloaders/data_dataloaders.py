5d148e6cfb3e33b5b663c3c279c101d9956da3b4
vpurushoth18 <vpurushoth97@gmail.com>
2024-08-13 16:59:27 +0530

    added code comment in dataloader_tea_test

--- dataloaders/data_dataloaders.py
+++ dataloaders/data_dataloaders.py
@@ -33,7 +33,7 @@ def dataloader_tea_train(args, tokenizer):
     return dataloader, len(tea_dataset), train_sampler
 
 def dataloader_tea_test(args, tokenizer, subset="test"):
-    
+    # adding the dataloader test
     if args.task_type == "retrieval":
         DataSet_DataLoader = Tea_DataLoader
     else:
